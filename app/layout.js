6b240b69a56c902db37020eaefedc677b7c61a51
Micah Enoch <enochmicah3@.com>
2025-10-18 03:19:32 +1300

    User Role Update

--- app/layout.js
+++ app/layout.js
@@ -1,32 +1,12 @@
-import localFont from "next/font/local";
+import { Outfit } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "react-hot-toast";
 import SessionProviderWrapper from "@/components/SessionProviderWrapper";
 // ✅ Load your local fonts
-const outfit = localFont({
-  src: [
-    {
-      path: "../public/fonts/Outfit-regular.ttf",
-      weight: "300",
-      style: "normal",
-    },
-    {
-      path: "../public/fonts/Outfit-Medium.ttf",
-      weight: "400",
-      style: "normal",
-    },
-    {
-      path: "../public/fonts/Outfit-Bold.ttf",
-      weight: "500",
-      style: "normal",
-    },
-  ],
-  display: "swap", // improves font rendering
-  fallback: ["Inter","sans-serif"], // ✅ Fallback fonts
-  
-});
 
 
+
+const outfit = Outfit({ subsets: ['latin'], weight: ["300", "400", "500"] })
 export const metadata = {
   title: "SmartCleaning - Assigning Task",
   description: "Smart cleaning task assignment and tracking system ",
