32b52f9d9647208ac4bf3bca5216bfc20d431f81
Micah Enoch <enochmicah3@.com>
2025-10-22 02:36:33 +1300

    Linking

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,13 +4,9 @@ import { assets } from "@/assets/assets";
 import Link from "next/link";
 import Image from "next/image";// ✅ 
 import { useRouter } from "next/navigation";
-import LoginForm from "./LoginForm";
-import RegisterForm from "./RegisterForm";
 
 const Navbar = () => {
   const router = useRouter();
-  const [showRegister, setShowRegister] = useState(false);
-  const [showLogin, setShowLogin] = useState(false);
 
   return (
     <>
@@ -39,55 +35,43 @@ const Navbar = () => {
 
         <ul className="hidden md:flex items-center gap-4">
       
-          <button
-            onClick={() => setShowRegister(true)}
+          <Link href="/register"      
             className="flex items-center gap-2  font-normal hover:bg-slate-100  text-emerald-900/80 bg-yellow-500 rounded-full px-2 p-1  transition"
           >
             <Image src={assets.add} alt="user icon"  />
             Register
-          </button>
-          <button
-            onClick={() => setShowLogin(true)}
+          </Link>
+          <Link
+           href="/login"
             className="flex items-center gap-2 hover:text-yellow-400 transition px-2 p-1 text-slate-100"
           >
             <Image src={assets.key} alt="user icon" className=" hover:text-yellow-400 "   />
             Login
-          </button>
+          </Link>
         </ul>
 
         <div className="flex items-center md:hidden gap-2">
 
-          <button
-            onClick={() => setShowRegister(true)}
+          <Link
+          href="/register"
+           
             className="flex items-center gap-2 hover:bg-slate-100  text-emerald-900/80 bg-yellow-500 rounded-full px-2 p-2 transition"
           >
             <Image src={assets.user_icon} alt="user icon" 
  />
             Register
-          </button>
-          <button
-            onClick={() => setShowLogin(true)}
+          </Link>
+          <Link
+           href="/login"
             className="flex items-center gap-2 hover:text-yellow-400 px-2 p-2 transition"
           >
             <Image src={assets.user_icon} alt="user icon"  />
             Login
-          </button>
+          </Link>
         </div>
       </nav>
 
-      {/* Modal for Register Form */}
-      {showRegister && (
-        <RegisterForm
-          isOpen={showRegister}
-          onClose={() => setShowRegister(false)}
-        />
-      )}
-      {showLogin && (
-        <LoginForm
-          isOpen={showLogin}
-          onClose={() => setShowLogin(false)}
-        />
-      )}
+     
     </>
   );
 };
